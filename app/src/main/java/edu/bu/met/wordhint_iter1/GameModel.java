02cd72ee04b978da610991a38284479a2ada9e73
Josh Bond <josh@mrbond.com>
2018-11-30 22:30:53 -0600

    Singleton pattern to prevent creating multiple GameModels

--- app/src/main/java/edu/bu/met/wordhint_iter1/GameModel.java
+++ app/src/main/java/edu/bu/met/wordhint_iter1/GameModel.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Random;
 
 public class GameModel {
+    // Singleton Pattern
     private static GameModel instance = null;
 
     public static GameModel getInstance(Context context) {
